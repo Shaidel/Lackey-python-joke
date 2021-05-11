ddaef6b7b61e27c3cbf65cd94492151ac3b78158
Andrzej Krzyżanowski <58177070+Shaidel@users.noreply.github.com>
2021-05-12 00:26:31 +0200

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,28 @@
+#import lackey
+from lackey import * 
+#from keyboard import mouse
+
+
+click("search.png")
+wait(3)
+type("notepad{ENTER}")
+wait(5)
+type("Przybywam z odmetow Twojego garbage collector'a by przejac wladze nad tym komputerem. Jestem JAVA :D")
+
+click("format.png")
+type("w")
+
+click("file.png")
+type("s")
+
+click("desktop.png")
+
+wait(3)
+
+doubleClick("programowanie.png")
+
+#doubleClick("lackey.png")
+
+click("txt.png")
+
+type("zapisz_mnie{ENTER}")
